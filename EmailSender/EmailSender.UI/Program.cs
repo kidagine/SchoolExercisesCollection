450d0a40983f5484fe957da02cebd458d4c6e780
kidagine <36992613+kidagine@users.noreply.github.com>
2019-10-24 18:52:01 +0200

    +Added the email sender exercise.

---
+++ EmailSender/EmailSender.UI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace EmailSender.UI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//You have to enable less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps
+			Console.WriteLine("Sending email");
+			MailMessage mail = new MailMessage();
+			SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+			mail.From = new MailAddress("your-email-address@gmail.com");
+			mail.To.Add("address-to@hotmail.com");
+			mail.Subject = "Test email";
+			mail.Body = "This is a test email.";
+
+			SmtpServer.Port = 587;
+			SmtpServer.Credentials = new System.Net.NetworkCredential("your-email-address@gmail.com", "password");
+			SmtpServer.EnableSsl = true;
+
+			SmtpServer.Send(mail);
+			Console.WriteLine("Email send!");
+			Console.ReadLine();
+		}
+	}
+}
